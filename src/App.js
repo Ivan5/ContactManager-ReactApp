65c36cfa3b8417baf8a268e66faebfe9206c7156
Ivan5 <converse_ivan@hotmail.com>
2018-10-13 17:49:28 -0500

    Working with props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Contact from  './components/Contact';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
-        <Contact />
+        <Header branding="Contact Manager"/>
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="888-888-888"/>
+        <Contact name="Karen Smith" email="karen@gmail.com" phone="999-999-999"/>
       </div>
     );
   }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 class Contact extends Component {
   render() {
+    const { name, email, phone } = this.props;
     return (
       <div>
-        <h4>John Doe</h4>
+        <h4>{name}</h4>
         <ul>
-          <li>Email: jdoe@gamil.com</li>
-          <li>Phone: 555-555-555</li>
+          <li>Email: {email}</li>
+          <li>Phone: {phone}</li>
         </ul>
       </div>
     )
   }
+}
+
+
+Contact.protoTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
 }
 
 export default Contact;
