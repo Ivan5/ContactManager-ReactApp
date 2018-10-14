b882bb7e473238b9df1bc8ab6c10430c21a1a495
Ivan5 <converse_ivan@hotmail.com>
2018-10-14 17:30:18 -0500

    Add events to make some functions

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,20 +3,31 @@ import PropTypes from 'prop-types';
 
 
 class Contact extends Component {
+  state = {
+    showContactInfo : false
+  };
+
+  onShowClick = e => {
+    this.setState({
+      showContactInfo : !this.state.showContactInfo
+    });
+  }
+
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state; 
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
+        <h4>{name} <i onClick={this.onShowClick} className="fas fa-sort-down"></i></h4>
+        {showContactInfo ? (<ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        </ul>) : null}
+        
       </div>
     )
   }
 }
-
 
 Contact.protoTypes = {
   contact: PropTypes.object.isRequired,
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -28,10 +28,10 @@ class Contacts extends Component {
   render() {
     const { contacts } = this.state;
     return (
-      <div>
+      <React.Fragment>
         { contacts.map(contact => (<Contact key={contact.id} contact={contact}/>
         ))}
-      </div>
+      </React.Fragment>
     )
   }
 }
