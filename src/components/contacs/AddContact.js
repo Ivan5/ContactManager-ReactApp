2b15d54a6ad447ad6f96c6bacf2da6c4763cf3f3
Ivan5 <converse_ivan@hotmail.com>
2018-10-16 17:59:30 -0500

    Add onChange func in form

--- src/components/contacs/AddContact.js
+++ src/components/contacs/AddContact.js
@@ -5,7 +5,15 @@ class AddContact extends Component {
     name: '',
     email: '',
     phone: ''
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
   }
+
+  onChange = e => this.setState({[e.target.name]: e.target.value});
+
   render() {
     const { name, email, phone } = this.state;
     return (
@@ -13,18 +21,18 @@ class AddContact extends Component {
         <div className="card-header">
         Add Contact</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
-              <input className="form-control from-control-lg" placeholder="Enter name" name="name" value={name}/>
+              <input className="form-control from-control-lg" placeholder="Enter name" name="name" value={name} onChange={this.onChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="email">Email</label>
-              <input className="form-control from-control-lg" placeholder="Enter a Email" name="email" value={email}/>
+              <input className="form-control from-control-lg" placeholder="Enter a Email" name="email" value={email} onChange={this.onChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="phone">Phone</label>
-              <input className="form-control from-control-lg" placeholder="Enter a Phone" name="phone" value={phone}/>
+              <input className="form-control from-control-lg" placeholder="Enter a Phone" name="phone" value={phone} onChange={this.onChange}/>
             </div>
             <input type="submit" value="Add Contact" className="btn btn-primary btn-block"/>
           </form>
