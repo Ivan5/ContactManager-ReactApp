c0779ed505fbd050a233dc5c64f446ce26b62442
Ivan5 <converse_ivan@hotmail.com>
2018-10-17 16:02:42 -0500

    Add react-route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Contacts from  './components/contacs/Contacts';
 import Header from './components/layout/Header';
+import About from './components/pages/About';
 import { Provider } from './contacs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddContact from './components/contacs/AddContact';
@@ -11,13 +12,18 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding="Contact Manager"/>
-          <div className="container">
-          <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager"/>
+            <div className="container">
+            <Switch>
+              <Route exact path="/" component={Contacts} />
+              <Route exact path="/contact/add" component={AddContact} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
